8c7f6163c45a85ffab23e11ba896c96aa16a8301
alejandro21102006 <alejandrochinoleon@gmail.com>
2024-06-12 11:27:17 -0500

    Update netflix.java

--- src/netflix.java
+++ src/netflix.java
@@ -8,7 +8,7 @@ public class netflix {
         String apellidos,correo,correo_in,contraseña,contraseña_in;
         int contador=0;
 
-        System.out.println("____                          " +
+        System.out.println("_____                        " +
                 "⣶⡄⠀⣶⠀⢰⣶⣶⡆⠠⣶⣶⣶⡆⢰⣶⣶⣦⠀⣶⠀⠀⠀⣶⡆⠐⣶⠀⣰⡖\n" +
                 "⣿⣷⡀⣿⠀⣸⣇⣀⡀⠀⠀⣿⠀⠀⢸⣇⣀⡀⠀⣿⠀⠀⠀⣿⡇⠀⠹⣷⡿⠁\n" +
                 "⣿⡿⣷⣿⠀⣿⡟⠛⠁⠀⠀⣿⠀⠀⢸⡏⠉⠁⠀⣿⠀⠀⠀⣿⡇⠀⢠⣿⣇⠀\n" +
