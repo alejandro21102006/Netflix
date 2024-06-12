e7cf29bdd21693b23f57f551c44f24027a1674bb
alejandro21102006 <alejandrochinoleon@gmail.com>
2024-06-11 23:05:31 -0500

    Update netflix.java

--- src/netflix.java
+++ src/netflix.java
@@ -1,6 +1,6 @@
-import java.util.Scanner;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Scanner;
 public class netflix {
     private String nombre,nombreUsuario;
     private int opcion;
@@ -64,13 +64,35 @@ public class netflix {
 
         }
         if (opcion < 3){
-            this.fprueva();
+            this.Pago();
+            this.manejarPerfiles();
+            this.catalogoDePelicula();
         }
         else {
-            System.out.println("intenta de buevo:");
+            System.out.println("inteta de buevo:");
         }
         return opcion;
     }
+    public String Pago() {
+        Scanner xd= new Scanner(System.in);
+        System.out.println("El metodo de pago es por tarjeta de debito o credito");
+        System.out.println("A continuacion ingrese los datos solicitados para completar el pago de su membresia");
+        int telefono,cvv,ntarjeta;
+        String correo1,fechadevencimiento;
+        System.out.println("Ingrese su número de tarjeta");
+        ntarjeta= xd.nextInt();
+        xd.nextLine();
+        System.out.println("Ingrese su fecha de caducidad");
+        fechadevencimiento= xd.nextLine();
+        System.out.println("Ingrese su CVV");
+        cvv= xd.nextInt();
+        System.out.println("...procesando...");
+        System.out.println("...procesando...");
+        System.out.println("Su pago fue procesado correctamente!");
+        System.out.println("Ya puede disfrutar del amplio catalogo de Netflix");
+        return null;
+    }
+
 
     public String manejarPerfiles() {
         Scanner gab = new Scanner(System.in);
@@ -173,50 +195,195 @@ public class netflix {
     }
 
     public String catalogoDePelicula() {
-        Scanner categoria = new Scanner(System.in);
-        String anime, terror, comedia, accion;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("⣶⡄⠀⣶⠀⢰⣶⣶⡆⠠⣶⣶⣶⡆⢰⣶⣶⣦⠀⣶⠀⠀⠀⣶⡆⠐⣶⠀⣰⡖\n" +
+                "⣿⣷⡀⣿⠀⣸⣇⣀⡀⠀⠀⣿⠀⠀⢸⣇⣀⡀⠀⣿⠀⠀⠀⣿⡇⠀⠹⣷⡿⠁\n" +
+                "⣿⡿⣷⣿⠀⣿⡟⠛⠁⠀⠀⣿⠀⠀⢸⡏⠉⠁⠀⣿⠀⠀⠀⣿⡇⠀⢠⣿⣇⠀\n" +
+                "⣿⡇⢻⣿⠀⣿⣷⣶⡆⠀⠀⠿⠀⠀⠸⠇⠀⠀⠀⣿⣶⣶⠀⣿⡇⢀⣿⠋⢿⡆\n" +
+                "⠉⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠉");
         System.out.println("-----Elija una opción-----");
-        System.out.println("Categoria");
-
+        System.out.println("Categoría");
         System.out.println("1. Anime");
         System.out.println("2. Terror");
         System.out.println("3. Comedia");
         System.out.println("4. Acción");
-        System.out.println("Por favor, elija una categoria (ingrese el número correspondiente): ");
-        int opcionCategoria = categoria.nextInt();
-        categoria.nextLine();
+
+        int opcionCategoria = sc.nextInt();
+        sc.nextLine(); // Limpiar el buffer
+
         switch (opcionCategoria) {
             case 1:
-                System.out.println("elija una pelicula de accion:");
-                System.out.println("- Inception");
-                System.out.println("- Jurassic Park");
-                System.out.println("- Indiana Jones: Raiders of the Lost Ark");
+                System.out.println("Ha elegido una película de anime:");
+                System.out.println("1 One Piece");
+                System.out.println("2 Shingeki no Kyojin");
+                System.out.println("3 Kimetsu no Yaiba");
+                int descripcion = sc.nextInt();
+                sc.nextLine();
+                switch (descripcion){
+                    case 1:
+                        System.out.println("One Piece");
+                        System.out.println("año:1997");
+                        System.out.println("director:Eiichiro Oda");
+                        System.out.println("descripcion:Esta larga serie sigue las aventuras de Monkey D. Luffy y " +
+                                "su tripulación pirata mientras buscan el legendario tesoro conocido como \"One Piece\" y " +
+                                "navegan por mares peligrosos. ");
+                        break;
+                    case 2:
+                        System.out.println("Shingeki no Kyojin");
+                        System.out.println("año:2009");
+                        System.out.println("director:Hajime Isayama");
+                        System.out.println("Shingeki no Kyojin presenta un mundo sombrío donde la humanidad " +
+                                "se enfrenta a la extinción debido a la aparición de gigantes humanoides devoradores" +
+                                " de humanos conocidos como \"Titanes\". La trama sigue a Eren Yeager, Mikasa " +
+                                "Ackerman y Armin Arlert, quienes, tras presenciar la devastación causada por los " +
+                                "Titanes en su infancia, se unen al Cuerpo de Exploración para luchar contra ellos y " +
+                                "descubrir los secretos detrás de su origen.");
+                        break;
+                    case 3:
+                        System.out.println("Kimetsu no Yaiba");
+                        System.out.println("año:2016");
+                        System.out.println("director:Koyoharu Gotouge");
+                        System.out.println("\"Kimetsu no Yaiba\" sigue a Tanjiro Kamado, un joven que se " +
+                                "convierte en un cazador de demonios después de que su familia es brutalmente " +
+                                "asesinada por un demonio, y su hermana Nezuko es convertida en uno. Con la " +
+                                "determinación de vengar a su familia y encontrar una cura para Nezuko, Tanjiro " +
+                                "se une a la organización de cazadores de demonios y se embarca en una emocionante " +
+                                "búsqueda llena de acción y sacrificio.");
+                        break;
+                    default:
+                        System.out.println("recuerda escoger uno");
+                }
+
                 break;
             case 2:
-                System.out.println("Ha elegido una pelicula de anime:");
-                System.out.println("- Sord Arta Online ");
-                System.out.println("- Shingeki no Kiojin");
-                System.out.println("- Kimetsu no yaiba");
+                System.out.println("Ha elegido una película de terror:");
+                System.out.println("1 El conjuro");
+                System.out.println("2 Annabelle");
+                System.out.println("3 La monja");
+                int descripcion1 = sc.nextInt();
+                sc.nextLine();
+                switch (descripcion1){
+                    case 1:
+                        System.out.println("El conjuro");
+                        System.out.println("año:2013");
+                        System.out.println("director: James Wan");
+                        System.out.println("The Conjuring es un escalofriante thriller sobrenatural que " +
+                                "sigue a la familia Perron mientras se mudan a una casa en Rhode Island " +
+                                "en la década de 1970, solo para encontrarse acechados por entidades " +
+                                "malévolas. Desesperados, llaman a los investigadores paranormales Ed " +
+                                "y Lorraine Warren para ayudarlos.");
+                        break;
+                    case 2:
+                        System.out.println("Annabelle");
+                        System.out.println("año:2014");
+                        System.out.println("director: John R. Leonetti");
+                        System.out.println("Annabelle es un inquietante thriller de terror que sigue la " +
+                                "historia de una pareja joven, Mia y John, quienes comienzan a experimentar " +
+                                "fenómenos aterradores después de que John le regala a Mia una muñeca Annabelle " +
+                                "para su colección. Pronto descubren que la muñeca está poseída por una entidad " +
+                                "maligna que desata el caos en sus vidas y amenaza su seguridad. ");
+                        break;
+                    case 3:
+                        System.out.println("La monja");
+                        System.out.println("año:2018");
+                        System.out.println("director:Corin Hardy");
+                        System.out.println("La Monja es un escalofriante thriller de terror que sigue la " +
+                                "historia de un sacerdote y una joven novicia enviados por el Vaticano " +
+                                "para investigar un convento en Rumania, donde una monja se suicidó " +
+                                "misteriosamente. Pronto descubren que el convento está habitado " +
+                                "por una presencia demoníaca malévola que se manifiesta en forma de " +
+                                "una monja siniestra.");
+                        break;
+                    default:
+                        System.out.println("recuerda escoger uno");
+                }
+
                 break;
             case 3:
-                System.out.println("Ha elegido una pelicula de terror:");
-                System.out.println("- el conjuro ");
-                System.out.println("- anabelle");
-                System.out.println("- la monja");
+                System.out.println("Ha elegido una película de comedia:");
+                System.out.println("- La máscara");
+                System.out.println("- Asu Mare");
+                System.out.println("- Todo poderoso");
+                int descripcion2 = sc.nextInt();
+                sc.nextLine();
+                switch (descripcion2){
+                    case 1:
+                        System.out.println("La máscara");
+                        System.out.println("año:1994");
+                        System.out.println("director:Chuck Russell");
+                        System.out.println("La Máscara es una comedia de acción que sigue a Stanley " +
+                                "Ipkiss, un hombre tímido y desafortunado que encuentra una máscara " +
+                                "antigua que le otorga habilidades sobrenaturales y lo transforma en " +
+                                "un personaje extrovertido y cómico.");
+                        break;
+                    case 2:
+                        System.out.println("Asu Mare");
+                        System.out.println("año:2013");
+                        System.out.println("director:Ricardo Maldonado");
+                        System.out.println("Asu Mare es una comedia que sigue la vida de Cachín, un joven " +
+                                "peruano que vive en San Isidro, Lima. La película relata sus divertidas " +
+                                "experiencias y desafíos mientras crece en un entorno familiar humilde. ");
+                        break;
+                    case 3:
+                        System.out.println("Todo poderoso");
+                        System.out.println("año:2003");
+                        System.out.println("director:Tom Shadyac");
+                        System.out.println("Todo Poderoso es una comedia que sigue a Bruce Nolan, un " +
+                                "reportero de televisión frustrado que, después de un día particularmente " +
+                                "malo, se queja con Dios por su vida. ");
+                        break;
+                    default:
+                        System.out.println("recuerda escoger uno");
+                }
+
                 break;
             case 4:
-                System.out.println("Ha elegido una pelicula de comedia:");
-                System.out.println("- la mascara");
-                System.out.println("- asu mare ");
-                System.out.println("- todo póderoso");
+                System.out.println("Ha elegido una película de acción:");
+                System.out.println("1 Inception");
+                System.out.println("2 Jurassic Park");
+                System.out.println("3 Gladiator");
+                int descripcion3 = sc.nextInt();
+                sc.nextLine();
+                switch (descripcion3){
+                    case 1:
+                        System.out.println("incepcion");
+                        System.out.println("año:2010");
+                        System.out.println("director:Christopher Nolan");
+                        System.out.println("Inception es un thriller de ciencia ficción que sigue a " +
+                                "Dom Cobb, un experto en el arte de extraer secretos del subconsciente " +
+                                "de las personas a través de los sueños. ");
+                        break;
+                    case 2:
+                        System.out.println("Jurassic Park");
+                        System.out.println("año:1993");
+                        System.out.println("director:Steven Spielberg");
+                        System.out.println("Jurassic Park cuenta la historia del Dr. John Hammond, quien " +
+                                "crea un parque temático de dinosaurios en una isla remota. Invita a un " +
+                                "grupo de personas a visitar el parque para evaluar su viabilidad, pero " +
+                                "todo se complica cuando una serie de fallos técnicos y un sabotaje liberan " +
+                                "a los peligrosos dinosaurios. ");
+                        break;
+                    case 3:
+                        System.out.println("Gladiator");
+                        System.out.println("año:2000");
+                        System.out.println("director:Ridley Scott ");
+                        System.out.println(" Protagonizada por Russell Crowe, esta película sigue " +
+                                "a un general romano que es traicionado por el hijo del emperador y " +
+                                "se convierte en un gladiador en busca de venganza.");
+                        System.out.println("__GRACIAS__");
+                        break;
+                    default:
+                        System.out.println("recuerda escoger uno");
+                }
 
+                break;
             default:
                 System.out.println("Opción no válida.");
                 break;
         }
         return null;
-
     }
+
     public static void main(String[] args) {
         System.out.println("----BIENVENIDO----");
         netflix NETFLIX= new netflix();
