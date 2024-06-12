001b129350f253aa0355dd5ce4de696ea2fed66d
alejandro21102006 <alejandrochinoleon@gmail.com>
2024-06-12 09:25:35 -0500

    Update netflix.java

--- src/netflix.java
+++ src/netflix.java
@@ -11,7 +11,7 @@ public class netflix {
         System.out.println("NETFLIX ");
         System.out.println("-----ELIJA UNA OPCIÓN-----");
         System.out.println("1. Registrarse");
-        System.out.println("2. Iniciar sesión");
+        System.out.println("2. Iniciar sesión-");
         System.out.print("Selecciona una opción: ");
         int opcion = datos.nextInt();
         datos.nextLine();
