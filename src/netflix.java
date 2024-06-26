79bf7a7b57cba9c63aac3ab2e89a072339aa9e52
alejandro21102006 <alejandrochinoleon@gmail.com>
2024-06-26 08:45:06 -0500

    Update netflix.java

--- src/netflix.java
+++ src/netflix.java
@@ -14,7 +14,7 @@ public class netflix {
                 "⣿⡿⣷⣿⠀⣿⡟⠛⠁⠀⠀⣿⠀⠀⢸⡏⠉⠁⠀⣿⠀⠀⠀⣿⡇⠀⢠⣿⣇⠀\n" +
                 "⣿⡇⢻⣿⠀⣿⣷⣶⡆⠀⠀⠿⠀⠀⠸⠇⠀⠀⠀⣿⣶⣶⠀⣿⡇⢀⣿⠋⢿⡆\n" +
                 "⠉⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠉");
-        System.out.println("------NETFLIX--------");
+        System.out.println("------NETFLIX-------");
         System.out.println("----ELIJA UNA OPCIÓN----");
         System.out.println("1. Registrarse");
         System.out.println("2. Iniciar sesión");
